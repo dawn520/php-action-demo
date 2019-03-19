eb6fb0296ef75264ba35b10c4b6aa9b2897eebc5
xixi zhou <admin@zhouchenxi.cn>
2019-03-19 16:59:34 +0800

    剑指offer 003-二维数组中的查找

---
+++ src/offer/003/solution1.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xixi
+ * Date: 2019/3/19
+ * Time: 16:17
+ * Description: 分治法（不使用php数组函数）从左下角开始
+ * @param $target
+ * @param $array
+ * @return bool
+ */
+function Find($target, $array)
+{
+    $arrayLength = count($array);
+    $y = 0;
+    $x = $arrayLength - 1;
+    while ($x >= 0 && $y <count($array[0])) {
+        if ($target == $array[$x][$y]) {
+            return true;
+        }
+        if ($target > $array[$x][$y]) {
+            $y++;
+        } else {
+            $x--;
+        }
+    }
+    return false;
+}
+
+$a = [
+    [1, 2, 8, 9],
+    [2, 4, 9, 12],
+    [4, 7, 10, 13],
+    [6, 8, 11, 15]
+];
+
+var_dump(find(11,$a));
